cc9e3fcea903769b34b445031a041675ff698dea
RefubuS <refubuspl@gmail.com>
2023-10-25 21:22:25 +0200

    Refactor FavouriteServlet.java, żeby tworzył pliki typu Gson (Google JSON)

--- backend/src/main/java/com/example/backend/Favourite/FavouriteServlet.java
+++ backend/src/main/java/com/example/backend/Favourite/FavouriteServlet.java
@@ -24,6 +24,12 @@ public class FavouriteServlet extends HttpServlet {
             FavouriteDTO favourite = favouriteDAO.getFavouriteById(id);
             request.setAttribute("favourite", favourite);
             request.getRequestDispatcher("/favourite.jsp").forward(request, response);
+
+            Gson gson = new Gson();
+            String json = gson.toJson(favourite);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
         } else if ("create".equals(action)) {
             Long userId = Long.parseLong(request.getParameter("userId"));
             Long productId = Long.parseLong(request.getParameter("productId"));
@@ -31,6 +37,12 @@ public class FavouriteServlet extends HttpServlet {
 
             favouriteDAO.createFavourite(newFavourite);
             response.sendRedirect(request.getContextPath() + "/favourite?action=get&id=" + newFavourite.getId());
+
+            Gson gson = new Gson();
+            String json = gson.toJson(newFavourite);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
         } else if ("update".equals(action)) {
             Long id = Long.parseLong(request.getParameter("id"));
             Long userId = Long.parseLong(request.getParameter("userId"));
@@ -39,11 +51,24 @@ public class FavouriteServlet extends HttpServlet {
 
             favouriteDAO.updateFavourite(updatedFavourite);
             response.sendRedirect(request.getContextPath() + "/favourite?action=get&id=" + id);
+
+            Gson gson = new Gson();
+            String json = gson.toJson(updatedFavourite);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
         } else if ("delete".equals(action)) {
             Long id = Long.parseLong(request.getParameter("id"));
 
             favouriteDAO.deleteFavourite(id);
             response.sendRedirect(request.getContextPath() + "/favourite?action=list"); // Redirect to a list page or another appropriate location.
+
+            Gson gson = new Gson();
+            String message = "Favorite deleted successfully.";
+            String json = gson.toJson(message);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
         } else {
             // Handle other actions or provide an error message.
         }
